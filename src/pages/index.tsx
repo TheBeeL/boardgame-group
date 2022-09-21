86e7d30bb0ff8471553d8f39c1ead7de5f44d39f
Thomas Cipriani <the.beel.616@gmail.com>
2022-09-21 19:30:10 +0200

    Cleaned home from boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,21 +4,7 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import { trpc } from "../utils/trpc";
 
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
-
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
-  const { data: session } = useSession();
-  const router = useRouter();
-
-  if (session) {
-    router.push("/collection");
-  }
-
   return (
     <>
       <Head>
@@ -27,29 +13,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4"></main>
+      <main className="container mx-auto"></main>
     </>
-  );
-};
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm text-violet-500 underline decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
   );
 };
 
