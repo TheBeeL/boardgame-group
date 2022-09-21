49da2d403203ebc173ea1629ec5de7022ae07c3c
Thomas Cipriani <the.beel.616@gmail.com>
2022-09-21 19:13:47 +0200

    Created user update mutation

--- src/server/router/user.ts
+++ src/server/router/user.ts
@@ -1,7 +1,20 @@
+import { z } from "zod";
 import { createProtectedRouter } from "./protected-router";
 
-export const userRouter = createProtectedRouter().query("get", {
-  resolve: async ({ ctx: { prisma, session } }) => {
-    return await prisma.user.findUnique({ where: { id: session.user.id } });
-  },
-});
+export const userRouter = createProtectedRouter()
+  .query("get", {
+    resolve: async ({ ctx: { prisma, session } }) => {
+      return await prisma.user.findUnique({ where: { id: session.user.id } });
+    },
+  })
+  .mutation("update", {
+    input: z.object({
+      bggUsername: z.string(),
+    }),
+    resolve: async ({ input, ctx: { prisma, session } }) => {
+      await prisma.user.update({
+        where: { id: session.user.id },
+        data: input,
+      });
+    },
+  });
