9684cf78b457ae4b39fd2ada8257b5394560e403
Thomas Cipriani <the.beel.616@gmail.com>
2022-10-05 20:04:48 +0200

    Purged JOYUI from UserListItem

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,6 @@ export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["cf.geekdo-images.com"],
+    domains: ["cf.geekdo-images.com", "lh3.googleusercontent.com"],
   },
 });
