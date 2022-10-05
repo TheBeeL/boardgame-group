c4d14fbcbbbd96addcf481abe7e96abcb8d70ca1
Thomas Cipriani <the.beel.616@gmail.com>
2022-10-05 20:59:22 +0200

    Refactored HoverEffect from BGCard component

--- src/components/BoardgameCard/BoardgameCard.tsx
+++ src/components/BoardgameCard/BoardgameCard.tsx
@@ -1,22 +1,10 @@
-import {
-  AspectRatio,
-  Box,
-  Card,
-  CardContent,
-  CardCover,
-  CardOverflow,
-} from "@mui/joy";
+import HoverEffect from "@components/BoardgameCard/HoverEffect";
+import { Box, Card, CardContent, CardCover } from "@mui/joy";
 import { Boardgame } from "@prisma/client";
-import { useGesture } from "@use-gesture/react";
 import Image from "next/image";
 import Link from "next/link";
-import { CSSProperties, useRef } from "react";
-import { useSpring, animated, to } from "react-spring";
+import { CSSProperties } from "react";
 import BoardgameTitle from "./BoardgameTitle";
-
-const calcX = (y: number, ly: number) =>
-  -(y - ly - window.innerHeight / 2) / 20;
-const calcY = (x: number, lx: number) => (x - lx - window.innerWidth / 2) / 20;
 
 interface BoardgameCardProps {
   boardgame: Boardgame;
@@ -29,53 +17,8 @@ const BoardgameCard = ({
   className = "",
   style,
 }: BoardgameCardProps) => {
-  const ref = useRef<HTMLDivElement>(null);
-  const [{ rotateX, rotateY, rotateZ, scale, zIndex }, api] = useSpring(() => ({
-    rotateX: 0,
-    rotateY: 0,
-    rotateZ: 0,
-    scale: 1,
-    zIndex: 0,
-    config: {
-      mass: 5,
-      tension: 350,
-      friction: 40,
-    },
-  }));
-
-  useGesture(
-    {
-      onMove: ({ event: { clientX, clientY } }) => {
-        if (!ref.current) return;
-        const { top, left, height, width } =
-          ref.current.getBoundingClientRect();
-
-        api({
-          rotateX: -(clientY - (top + height / 2)) / 15,
-          rotateY: (clientX - (left + width / 2)) / 15,
-          scale: 1.2,
-          zIndex: 10,
-        });
-      },
-      onHover: ({ hovering }) =>
-        !hovering && api({ rotateX: 0, rotateY: 0, scale: 1, zIndex: 0 }),
-    },
-    { target: ref, eventOptions: { passive: false } },
-  );
-
   return (
-    <animated.div
-      ref={ref}
-      className={`${className} aspect-square overflow-hidden rounded-lg hover:shadow-lg`}
-      style={{
-        transform: "perspective(600px)",
-        scale,
-        rotateX,
-        rotateY,
-        rotateZ,
-        zIndex: to([zIndex], (z) => Math.floor(z)),
-      }}
-    >
+    <HoverEffect className={`${className}`}>
       <Link href={`/boardgame/${boardgame.id}`}>
         <a>
           <Card
@@ -96,7 +39,7 @@ const BoardgameCard = ({
           </Card>
         </a>
       </Link>
-    </animated.div>
+    </HoverEffect>
   );
 };
 
---
+++ src/components/BoardgameCard/HoverEffect.tsx
@@ -0,0 +1,61 @@
+import { useGesture } from "@use-gesture/react";
+import { useRef } from "react";
+import { useSpring, animated, to } from "react-spring";
+
+interface HoverEffectProps {
+  className?: string;
+  children?: React.ReactNode;
+}
+
+const HoverEffect = ({ className = "", children }: HoverEffectProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [{ rotateX, rotateY, rotateZ, scale, zIndex }, api] = useSpring(() => ({
+    rotateX: 0,
+    rotateY: 0,
+    rotateZ: 0,
+    scale: 1,
+    zIndex: 0,
+    config: {
+      mass: 5,
+      tension: 350,
+      friction: 40,
+    },
+  }));
+
+  useGesture(
+    {
+      onMove: ({ event: { clientX, clientY } }) => {
+        if (!ref.current) return;
+        const { top, left, height, width } =
+          ref.current.getBoundingClientRect();
+
+        api({
+          rotateX: -(clientY - (top + height / 2)) / 15,
+          rotateY: (clientX - (left + width / 2)) / 15,
+          scale: 1.2,
+          zIndex: 10,
+        });
+      },
+      onHover: ({ hovering }) =>
+        !hovering && api({ rotateX: 0, rotateY: 0, scale: 1, zIndex: 0 }),
+    },
+    { target: ref, eventOptions: { passive: false } },
+  );
+  return (
+    <animated.div
+      ref={ref}
+      className={`${className} aspect-square overflow-hidden rounded-lg hover:shadow-lg`}
+      style={{
+        transform: "perspective(600px)",
+        scale,
+        rotateX,
+        rotateY,
+        rotateZ,
+        zIndex: to([zIndex], (z) => Math.floor(z)),
+      }}
+    >
+      {children}
+    </animated.div>
+  );
+};
+export default HoverEffect;
